fe25ee88eca03ca084c6cb729c14595a0f1bcd39
TiffaJenkins <100137572+TiffaJenkins@users.noreply.github.com>
2022-05-25 19:25:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+//document.querySelectorAll('.radio-button');
+
+function changeContent1() {
+    document.getElementById("show1").classList.remove("hide");
+    document.getElementById("show2").classList.add("hide");
+    document.getElementById("show3").classList.add("hide");
+    document.getElementById("show4").classList.add("hide");
+}
+function changeContent2() {
+    document.getElementById("show1").classList.add("hide");
+    document.getElementById("show2").classList.remove("hide");
+    document.getElementById("show3").classList.add("hide");
+    document.getElementById("show4").classList.add("hide");
+}
+function changeContent3() {
+    document.getElementById("show1").classList.add("hide");
+    document.getElementById("show2").classList.add("hide");
+    document.getElementById("show3").classList.remove("hide");
+    document.getElementById("show4").classList.add("hide");
+}
+function changeContent4() {
+    document.getElementById("show1").classList.add("hide");
+    document.getElementById("show2").classList.add("hide");
+    document.getElementById("show3").classList.add("hide");
+    document.getElementById("show4").classList.add("hide");
+}
